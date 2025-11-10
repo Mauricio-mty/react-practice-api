35a7f51a66e4032dad6db8a7d23df98ce4413de8
Mauricio-mty <00000422@uca.edu.sv>
2025-11-10 09:53:07 -0600

    Service user ✨

---
+++ src/service/UserService.js
@@ -0,0 +1,66 @@
+const User = require('../model/User');
+const sequelize = require('../config/db');
+
+const UserServices = {};
+
+UserServices.createUser = async (userData) => {
+    try{
+        const new_user = await User.create(userData);
+        return new_user;
+    }catch(error){
+        console.error('Error creating user:', error);
+        throw new Error("No se pudo crear el usuario");
+    }
+}
+
+UserServices.getUserById = async (userId) => {
+    try{
+        const user = await User.findByPk(userId);
+        return {message:"Usuario encontrado",user};
+    }catch(error){
+        console.error('Error fetching user by ID:', error);
+        throw new Error("No se pudo obtener el usuario");
+    }
+}
+
+UserServices.getAllUsers = async () => {
+    try{
+        const users = await User.findAll();
+        return {message:"Listado usuarios",users};
+    }catch(error){
+        console.error('Error fetching all users:', error);
+        throw new Error("No se pudieron obtener los usuarios");
+    }
+}
+
+UserServices.updateUser = async (userId, updateData) => {
+    try{
+        const data= await User.findByPk(userId);
+        if(!data){
+            throw new Error ('Usuario no encontrado');
+        }
+        await data.update(updateData);
+        return data;
+    }
+    catch(error){
+        console.error('Error updating user:', error);
+        throw new Error("No se pudo actualizar el usuario");
+    }
+}
+
+UserServices.deleteUser = async (userId) => {
+    try{
+        const data= await User.findByPk(userId);
+        if(!data){
+            throw new Error ('Usuario no encontrado');
+        }
+        const deletedUser = data.get({ plain: true });
+        await data.destroy();   
+        return {message:'Usuario eliminado correctamente', deletedUser};
+    }
+    catch(error){   
+        console.error('Error deleting user:', error);
+        throw new Error("No se pudo eliminar el usuario");
+    }
+}
+module.exports = UserServices;
