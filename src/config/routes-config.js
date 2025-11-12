648791b053553fee9e5f09b35f2c7c9c8bf975fa
Mauricio-mty <00000422@uca.edu.sv>
2025-11-12 09:16:18 -0600

    routes configuration with auth �️�️

--- src/config/routes-config.js
+++ src/config/routes-config.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const User = require("../routes/UserRoutes");
 const Escrito = require("../routes/EscritoRoutes");
+const  Login  = require("../routes/Login");
 
 const configRoutes = (app) => {
     app.use(express.json());
-
+    
+    app.use("/login",Login);
     app.use("/user",User);
     app.use("/escrito",Escrito);
+    
 };
 
 module.exports=configRoutes;
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/UserController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/',UserController.getAll);
-router.get('/:id',UserController.getById);
-
 router.post('/',UserController.create);
 
-router.put('/:id',UserController.update);
-
-router.delete('/:id',UserController.delete);
+//Private Routes
+router.get('/:id',authMiddleware,UserController.getById);
+router.put('/:id',authMiddleware,UserController.update);
+router.delete('/:id',authMiddleware,UserController.delete);
 
 module.exports=router
